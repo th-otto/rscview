f877881667a0e5e6801e940b8457e0f078a60345
Thorsten Otto <admin@tho-otto.de>
2023-12-21 18:59:24 +0100

    Update libcwrap.h for glibc 2.38

--- src/rscview.c
+++ src/rscview.c
@@ -1392,40 +1392,7 @@ static void print_version(void)
 
 /* ------------------------------------------------------------------------- */
 
-#if defined(__LINUX_GLIBC_WRAP_H)
-
-/* ugly hack to get __libc_start_main versioned */
-
-#if __GLIBC_PREREQ(2, 34)
-
-#define STR_(s) #s
-#define STR(s)  STR_(s)
-#include <dlfcn.h>
-
-#ifdef __UCLIBC__
-#define __libc_start_main       __uClibc_main
-#endif
-
-int __libc_start_main(
-        int (*main)(int,char**,char**), int ac, char **av,
-        int (*init)(void), void (*fini)(void),
-        void (*rtld_fini)(void), void *stack_end);
-int __libc_start_main(
-        int (*main)(int,char**,char**), int ac, char **av,
-        int (*init)(void), void (*fini)(void),
-        void (*rtld_fini)(void), void *stack_end)
-{
-	typeof(__libc_start_main) *real_lsm;
-	if ((*(void**)&real_lsm = dlsym(RTLD_NEXT, STR(__libc_start_main))) != 0)
-		return real_lsm(main, ac, av, init, fini, rtld_fini, stack_end);
-	fputs("BUG: dlsym error\n", stderr);
-	return 1;
-}
-#undef STR
-#undef STR_
-#endif
-#endif
-
+#include "linux/libcmain.h"
 
 int main(int argc, char **argv)
 {
